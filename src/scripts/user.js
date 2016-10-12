8e834fd6bb4a650ce6a58444ec0d59c7dba038e9
klimuwek <klimuwek@gmail.com>
2016-10-12 08:53:09 +0300

    in progress

--- src/scripts/user.js
+++ src/scripts/user.js
@@ -2,39 +2,52 @@
 
 $(document).ready(function() {
 
-	$("[type='search']").on("blur", function() {
-		var value = $(this).val(),
-			trimed = $.trim(value);
-		$(this).val(trimed);
+
+	$("body").removeClass('active');
+	$("#nav-trigger").on("click", function(event) {
+		event.stopPropagation();
+		$("body").addClass('active');
+	});
+	$("#menu").on("click", function(event) {
+		event.stopPropagation();
+	});
+	$("body").on("click", function() {
+		$("body").removeClass('active');
 	});
 
-	function getChar(e) {
-		if (e.which === null) {
-			if (e.keyCode < 32) return null;
-			return String.fromCharCode(e.keyCode);
-		}
-		if (e.which !== 0 && e.charCode !== 0) {
-			if (e.which < 32) return null;
-			return String.fromCharCode(e.which);
-		}
-		return null;
-	}
+	// $("[type='search']").on("blur", function() {
+	// 	var value = $(this).val(),
+	// 		trimed = $.trim(value);
+	// 	$(this).val(trimed);
+	// });
 
-	$("[type='search']").on("keypress", function(e) {
-		if (e.ctrlKey || e.altKey || e.metaKey) return;
-		var char = getChar(e);
-		if (!char) return;
-		this.value += char.toUpperCase();
-		return false;
-	});
+	// function getChar(e) {
+	// 	if (e.which === null) {
+	// 		if (e.keyCode < 32) return null;
+	// 		return String.fromCharCode(e.keyCode);
+	// 	}
+	// 	if (e.which !== 0 && e.charCode !== 0) {
+	// 		if (e.which < 32) return null;
+	// 		return String.fromCharCode(e.which);
+	// 	}
+	// 	return null;
+	// }
 
-	var input = document.querySelector("[type='search']");
-	input.addEventListener('invalid', function(e) {
-		if (input.validity.valueMissing) {
-			e.target.setCustomValidity("Заполните это поле"); 
-		} else if (!input.validity.valid) {
-			e.target.setCustomValidity("Введите данные в формате: А111АА111, А111АА11"); 
-		} 
-	}, false);
+	// $("[type='search']").on("keypress", function(e) {
+	// 	if (e.ctrlKey || e.altKey || e.metaKey) return;
+	// 	var char = getChar(e);
+	// 	if (!char) return;
+	// 	this.value += char.toUpperCase();
+	// 	return false;
+	// });
+
+	// var input = document.querySelector("[type='search']");
+	// input.addEventListener('invalid', function(e) {
+	// 	if (input.validity.valueMissing) {
+	// 		e.target.setCustomValidity("Заполните это поле"); 
+	// 	} else if (!input.validity.valid) {
+	// 		e.target.setCustomValidity("Введите данные в формате: А111АА111, А111АА11"); 
+	// 	} 
+	// }, false);
 
 });
